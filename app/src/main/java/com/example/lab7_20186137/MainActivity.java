12330aea8f3516658cc7d0098c31506f06866f6a
raykevinconcha <a20186137@pucp.edu.pe>
2023-11-18 12:39:03 -0500

    commit final

--- app/src/main/java/com/example/lab7_20186137/MainActivity.java
+++ app/src/main/java/com/example/lab7_20186137/MainActivity.java
@@ -1,26 +1,43 @@
 package com.example.lab7_20186137;
 
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btn_login;
+
     EditText email,password;
     FirebaseAuth mAuth;
+
+    ActivityResultLauncher<Intent> signInLauncher = registerForActivityResult(
+            new FirebaseAuthUIActivityResultContract(),
+            result -> {
+                if (result.getResultCode() == RESULT_OK) {
+                    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                    Log.d("msg-test", "Firebase uid: " + user.getUid());
+                } else {
+                    Log.d("msg-test", "Canceló el Log-in");
+                }
+            }
+    );
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +46,7 @@ public class MainActivity extends AppCompatActivity {
         email=findViewById(R.id.editTextTextEmailAddress);
         password=findViewById(R.id.editTextTextPassword);
         btn_login=findViewById(R.id.button1);
+
 
 
         btn_login.setOnClickListener(new View.OnClickListener() {

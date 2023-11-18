343cbfa65b2c89bf741bdc0810c50bd71d1ef45a
raykevinconcha <a20186137@pucp.edu.pe>
2023-11-18 12:11:25 -0500

    1

--- app/src/main/java/com/example/lab7_20186137/MainActivity.java
+++ app/src/main/java/com/example/lab7_20186137/MainActivity.java
@@ -1,14 +1,69 @@
 package com.example.lab7_20186137;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn_login;
+    EditText email,password;
+    FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth=FirebaseAuth.getInstance();
+        email=findViewById(R.id.editTextTextEmailAddress);
+        password=findViewById(R.id.editTextTextPassword);
+        btn_login=findViewById(R.id.button1);
+
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String emailUser=email.getText().toString().trim();
+                String passUser=password.getText().toString().trim();
+
+                if(emailUser.isEmpty() && passUser.isEmpty()){
+                    Toast.makeText(MainActivity.this,"Ingresar los datos",Toast.LENGTH_SHORT).show();
+                }else{
+                    loginUser(emailUser,passUser);
+                }
+            }
+
+            private void loginUser(String emailUser, String passUser) {
+
+                mAuth.signInWithEmailAndPassword(emailUser,passUser).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            finish();
+                            startActivity(new Intent(MainActivity.this, gestor.class));
+                        }else{
+                            Toast.makeText(MainActivity.this,"error",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(MainActivity.this, "Error de login", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+            }
+        });
     }
 }
